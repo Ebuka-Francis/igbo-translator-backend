170d3b22d5d69bc522dc8c11479dc89449817dc3
Ebuka-Francis <nwokefrancis99@gmail.com>
2025-06-17 16:27:33 +0100

    🔥

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOption = {
-   origin: 'https://translation-app-three-delta.vercel.app',
+   origin: [
+      'https://translation-app-three-delta.vercel.app',
+      '^https?://localhost(:d+)?$',
+   ],
 };
 
 // Middleware
