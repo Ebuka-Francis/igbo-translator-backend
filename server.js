a54239cc5dd8490159185c096ddd420541d480a5
Ebuka-Francis <nwokefrancis99@gmail.com>
2025-06-17 15:51:21 +0100

    🎨

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOption = {
-   origin: 'https://www.google.com/',
+   origin: 'https://translation-app-three-delta.vercel.app/',
 };
 
 // Middleware
